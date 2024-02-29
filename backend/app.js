fce7c478e43f7276c298d6c5a8a1cc9066aee721
Dzikri Muhammad <notfnd123@gmail.com>
2024-02-29 21:28:35 +0700

    feat: add migration, models, and sequelize config

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,28 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { Sequelize } = require("sequelize");
+require("dotenv").config();
 const app = express();
 
 app.use(bodyParser.json());
 
-app.listen(3000, () => {
-  console.log("Server is running on port: 3000");
-});
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    dialect: "postgres",
+    host: "localhost",
+  }
+);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    app.listen(3000, () => {
+      console.log("Server is running on port: 3000");
+    });
+  })
+  .catch(() => {
+    console.log("error");
+  });
