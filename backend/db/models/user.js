f61257dcbdb2c4e075d0360f748bb4e1c059ae7f
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-03 16:41:25 +0700

    feat: validation null and refresh_token

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -10,16 +10,29 @@ module.exports = (sequelize, DataTypes) => {
     static associate(models) {
       // define association here
       User.belongsTo(models.Role, {
-        foreignKey: id_role,
+        foreignKey: "id_role",
         as: "role",
       });
     }
   }
   User.init(
     {
-      username: DataTypes.STRING,
-      password: DataTypes.STRING,
-      id_role: DataTypes.INTEGER,
+      username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: DataTypes.STRING,
+        allowNull: false,
+      },
+      id_role: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+      },
+      refresh_token: {
+        type: DataTypes.STRING,
+        allowNull: true,
+      },
     },
     {
       sequelize,
