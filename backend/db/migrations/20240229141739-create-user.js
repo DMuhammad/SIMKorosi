a6ed09584c165cca8f945ff506b926b11817cbf3
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-03 16:40:49 +0700

    feat: add refresh_token

--- backend/db/migrations/20240229141739-create-user.js
+++ backend/db/migrations/20240229141739-create-user.js
@@ -21,6 +21,10 @@ module.exports = {
         allowNull: false,
         type: Sequelize.INTEGER,
       },
+      refresh_token: {
+        allowNull: true,
+        type: Sequelize.STRING,
+      },
       createdAt: {
         allowNull: false,
         type: Sequelize.DATE,
