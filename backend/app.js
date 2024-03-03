d1aaa1fe8fa2ed30380a0c6d4f3e6a71d49ddfed
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-03 16:38:09 +0700

    feat: add routes and cookie parser

--- backend/app.js
+++ backend/app.js
@@ -1,28 +1,16 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { Sequelize } = require("sequelize");
+const router = require("./routes");
+const cookieParser = require("cookie-parser");
 require("dotenv").config();
+
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cookieParser());
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USERNAME,
-  process.env.DB_PASSWORD,
-  {
-    dialect: "postgres",
-    host: "localhost",
-  }
-);
+app.use("/api", router);
 
-sequelize
-  .authenticate()
-  .then(() => {
-    app.listen(3000, () => {
-      console.log("Server is running on port: 3000");
-    });
-  })
-  .catch(() => {
-    console.log("error");
-  });
+app.listen(3000, () => {
+  console.log("Server is running on port: 3000");
+});
