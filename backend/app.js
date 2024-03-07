3afd0379df3d960b5fc45c077b447b89ca4311e2
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-07 13:23:16 +0700

    feat: add and implement cors

--- backend/app.js
+++ backend/app.js
@@ -1,16 +1,22 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const router = require("./routes");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
+const router = require("./routes");
 require("dotenv").config();
 
 const app = express();
+
+app.use(
+  cors({
+    credentials: true,
+    origin: "http://localhost:5173",
+  })
+);
 
 app.use(bodyParser.json());
 app.use(cookieParser());
 
 app.use("/api", router);
 
-app.listen(3000, () => {
-  console.log("Server is running on port: 3000");
-});
+app.listen(8000, () => console.log("Server is running on port: 8000"));
