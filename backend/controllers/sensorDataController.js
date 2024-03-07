ea18653518d13e5d0e825d49e066ae4581d75fba
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-07 13:22:01 +0700

    feat: controller sensor data

---
+++ backend/controllers/sensorDataController.js
@@ -0,0 +1,65 @@
+const db = require("../db/models");
+const { v4: uuidv4 } = require("uuid");
+
+const SensorData = db.sequelize.models.SensorData;
+
+module.exports = {
+  async getDatas(req, res) {
+    const limit = 10;
+    const { page } = req.query;
+    try {
+      const { count, rows: data } = await SensorData.findAndCountAll({
+        order: [["createdAt", "DESC"]],
+        limit: 10,
+        offset: (page - 1) * limit,
+      });
+
+      return res.status(200).json({
+        status: "success",
+        code: 200,
+        message: "Berhasil mengambil data sensor",
+        data,
+        pagination: {
+          page: parseInt(page),
+          limit,
+          total: count,
+          totalPages: Math.ceil(count / limit),
+        },
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: "error",
+        code: 400,
+        message: error.message,
+      });
+    }
+  },
+
+  async addNewData(req, res) {
+    const { suhu, kelembapan, ph, lokasi } = req.body;
+    try {
+      const data = await SensorData.create({
+        id: uuidv4(),
+        suhu,
+        kelembapan,
+        ph,
+        lokasi,
+      });
+
+      return res.status(201).json({
+        status: "success",
+        code: 201,
+        message: "Berhasil menambahkan data sensor",
+        data: {
+          id: data.id,
+        },
+      });
+    } catch (error) {
+      return res.status(400).json({
+        status: "error",
+        code: 400,
+        message: error.message,
+      });
+    }
+  },
+};
