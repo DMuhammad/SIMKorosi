6741a4343aebb8991d51c99d729df765acf9ced5
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-20 15:10:02 +0700

    feat: set up socket.io

--- backend/app.js
+++ backend/app.js
@@ -2,10 +2,14 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
+const { createServer } = require("http");
 const router = require("./routes");
+const socket = require("./utils/socket");
 require("dotenv").config();
 
 const app = express();
+const server = createServer(app);
+const io = socket.init(server);
 
 app.use(
   cors({
@@ -19,4 +23,8 @@ app.use(cookieParser());
 
 app.use("/api", router);
 
-app.listen(8000, () => console.log("Server is running on port: 8000"));
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(8000, () => console.log("Server is running on port: 8000"));
---
+++ backend/utils/socket.js
@@ -0,0 +1,19 @@
+const { Server } = require("socket.io");
+let io;
+
+module.exports = {
+  init: (httpServer) => {
+    io = new Server(httpServer, {
+      cors: {
+        origin: "http://localhost:5173",
+      },
+    });
+    return io;
+  },
+  getIO: () => {
+    if (!io) {
+      throw new Error("Socket.io not initialized");
+    }
+    return io;
+  },
+};
