de4adcbb7274cb85d6574ad4028ace6451815608
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-20 15:11:01 +0700

    feat: middleware to check auth

---
+++ backend/middleware/verifyToken.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const verifyToken = (req, res, next) => {
+  const { authorization } = req.headers;
+
+  if (!authorization) {
+    return res.status(401).json({ error: "Authorization token required" });
+  }
+
+  const token = authorization.split(" ")[1];
+
+  try {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET);
+
+    next();
+  } catch (error) {
+    res.status(401).json({ error: "Request is not authorized" });
+  }
+};
+
+module.exports = verifyToken;
