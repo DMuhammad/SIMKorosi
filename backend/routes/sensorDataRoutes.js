cefcc5ca44af03b073a8a5ff21c3f13035f9bd65
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-26 10:40:48 +0700

    feat: add middleware to post route

--- backend/routes/sensorDataRoutes.js
+++ backend/routes/sensorDataRoutes.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const sensorDataController = require("../controllers/sensorDataController");
 const verifyToken = require("../middleware/verifyToken");
+const verifySensor = require("../middleware/verifySensor");
 
 const sensorDataRoutes = Router();
 
 sensorDataRoutes.get("/", verifyToken, sensorDataController.getDatas);
-sensorDataRoutes.post("/data", sensorDataController.addNewData);
+sensorDataRoutes.post("/data", verifySensor, sensorDataController.addNewData);
 
 module.exports = sensorDataRoutes;
