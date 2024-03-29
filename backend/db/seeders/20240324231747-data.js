5516e687bd0ab6669531dc43eebbe9c63b892524
Dzikri Muhammad <notfnd123@gmail.com>
2024-03-30 05:32:38 +0700

    feat: add delay 1 sec

--- backend/db/seeders/20240324231747-data.js
+++ backend/db/seeders/20240324231747-data.js
@@ -3,25 +3,29 @@
 const { v4: uuidv4 } = require("uuid");
 const datas = [];
 
-for (let index = 0; index < 50; index++) {
-  const suhu = Number((Math.random() * 40).toFixed(1)) + 35;
-  const kelembapan = Number((Math.random() * 30).toFixed(1)) + 60;
-  const ph = Number((Math.random() * 10).toFixed(1)) + 3;
-  const id_lokasi = Number(Math.round(Math.random() * 3)) + 1;
+(async function generateData() {
+  for (let index = 0; index < 50; index++) {
+    const suhu = Number((Math.random() * 40).toFixed(1)) + 35;
+    const kelembapan = Number((Math.random() * 30).toFixed(1)) + 60;
+    const ph = Number((Math.random() * 10).toFixed(1)) + 3;
+    const id_lokasi = Number(Math.round(Math.random() * 3)) + 1;
 
-  datas.push({
-    id: uuidv4(),
-    id_sensor_suhu: "SS-S-01",
-    id_sensor_kelembapan: "SS-K-01",
-    id_sensor_ph: "SS-P-01",
-    suhu,
-    kelembapan,
-    ph,
-    id_lokasi,
-    created_at: new Date(),
-    updated_at: new Date(),
-  });
-}
+    datas.push({
+      id: uuidv4(),
+      id_sensor_suhu: "SS-S-01",
+      id_sensor_kelembapan: "SS-K-01",
+      id_sensor_ph: "SS-P-01",
+      suhu,
+      kelembapan,
+      ph,
+      id_lokasi,
+      created_at: new Date(),
+      updated_at: new Date(),
+    });
+
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  }
+})();
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
