27639461c30732c98a8ca089722043fc4f72fe2a
Dzikri Muhammad <notfnd123@gmail.com>
2024-05-15 13:02:54 +0700

    feat: update seeders (lokasi, korosi)

--- backend/db/seeders/20240324000225-lokasi.js
+++ backend/db/seeders/20240324000225-lokasi.js
@@ -32,6 +32,34 @@ module.exports = {
         created_at: new Date(),
         updated_at: new Date(),
       },
+      {
+        nama_lokasi: "Area E",
+        geo_x: 106.827147,
+        geo_y: -6.175393,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nama_lokasi: "Area F",
+        geo_x: 107.590615,
+        geo_y: -6.917464,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nama_lokasi: "Area G",
+        geo_x: 110.364445,
+        geo_y: -7.795586,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
+      {
+        nama_lokasi: "Area H",
+        geo_x: 112.743142,
+        geo_y: -7.257473,
+        created_at: new Date(),
+        updated_at: new Date(),
+      },
     ]);
   },
 
--- backend/db/seeders/20240324231747-data.js
+++ backend/db/seeders/20240324231747-data.js
@@ -1,14 +1,14 @@
 "use strict";
 
 const { v4: uuidv4 } = require("uuid");
-const datas = [];
 
-(async function generateData() {
-  for (let index = 0; index < 50; index++) {
+async function generateData() {
+  const datas = [];
+  for (let index = 0; index < 200; index++) {
     const suhu = Number((Math.random() * 40).toFixed(1)) + 35;
     const kelembapan = Number((Math.random() * 30).toFixed(1)) + 60;
     const ph = Number((Math.random() * 10).toFixed(1)) + 3;
-    const id_lokasi = Number(Math.round(Math.random() * 3)) + 1;
+    const id_lokasi = Number(Math.round(Math.random() * 7)) + 1;
 
     datas.push({
       id: uuidv4(),
@@ -25,11 +25,14 @@ const datas = [];
 
     await new Promise((resolve) => setTimeout(resolve, 1000));
   }
-})();
+
+  return datas;
+}
 
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
+    const datas = await generateData();
     return queryInterface.bulkInsert("data", datas);
   },
 
