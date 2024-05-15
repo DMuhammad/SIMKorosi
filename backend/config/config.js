1cce603d9cf0f8a4650769b699be85e90be8c276
Dzikri Muhammad <notfnd123@gmail.com>
2024-05-15 13:05:15 +0700

    feat: update config and env template for production

--- backend/config/config.js
+++ backend/config/config.js
@@ -16,10 +16,10 @@ module.exports = {
     dialect: "postgres",
   },
   production: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-    host: "127.0.0.1",
+    username: process.env.DB_PROD_USERNAME,
+    password: process.env.DB_PROD_PASSWORD,
+    database: process.env.DB_PROD_NAME,
+    host: process.env.DB_PROD_HOST,
     dialect: "postgres",
   },
 };
