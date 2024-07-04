bc36bc81b7d59d13e61edf8a242d30d77cd210e6
Dzikri Muhammad <notfnd123@gmail.com>
2024-07-04 14:12:31 +0700

    fix: bug models

--- backend/db/models/Lokasi.js
+++ backend/db/models/Lokasi.js
@@ -8,9 +8,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
-      this.hasMany(models.SensorLokasi, {
+      this.hasMany(models.Sensor, {
         foreignKey: "id_lokasi",
-        as: "sensorlokasi",
+        as: "sensor",
       });
     }
   }
