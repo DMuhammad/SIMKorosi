0d02b37a78f524faa25385c5651dda9bd1c1107f
Dzikri Muhammad <notfnd123@gmail.com>
2024-08-21 10:52:50 +0700

    fix: moment locale

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,6 +5,7 @@ import { AuthProvider } from "./context/AuthContext.jsx";
 import App from "./App.jsx";
 import "./index.css";
 import { NavigationProvider } from "./context/NavigationContext.jsx";
+import "moment/dist/locale/id";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
